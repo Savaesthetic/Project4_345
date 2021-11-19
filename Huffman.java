fd0bdad6d05409a64438d6dc8e318f8a33cf812c
Savaesthetic <asavaus@gmail.com>
2021-11-18 19:13:15 -0700

    Started Huffman.

--- Huffman.java
+++ Huffman.java
@@ -1,9 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Huffman {
     /***
      *    @param fn: the filename for the text you are compressing
      ***/
     public Huffman(String fn) {
-	//TO BE IMPLEMENTED
+        /* Read in file, for each character add to hashtable, once whole file has been read
+        * convert hashtable to minPQ, construct the tree with minPQ
+        */
+        try {
+            File input = new File(fn);
+            Scanner fileReader = new Scanner(input);
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error opening and reading file.");
+            e.printStackTrace();
+        }
     }
 
     /***
